f7d4e0d9d00a4cc1cc08025af21a05d20fe0d65d
tornike thcalidze <ttchalidze@gmail.com>
2025-04-03 10:08:49 -0400

    first1

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,47 @@
+import { useState } from "react";
+import PLANTS from "./data";
+import Cart from "./cart/cart";
+import Plants from "./Plants/plants";
+
 export default function App() {
-  return <></>;
+  const [cart, setCart] = useState;
+
+  const addToCart = (plant) => {
+    const itemExist = cart.find((i) => i.id === plant.id);
+    if (itemExist) {
+      setCart(
+        cart.map((item) =>
+          item.id === plant.id ? { ...item, quantity: item.quantity + 1 } : item
+        )
+      );
+    } else {
+      const item = { ...plant, quantity: 1 };
+      setCart([...cart, item]);
+    }
+  };
+  const removeFromCart = (removable) => {
+    setCart(
+      cart
+        .map((item) =>
+          item.id === removable.id
+            ? { ...item, quantity: item.quantity - 1 }
+            : item
+        )
+        .filter((item) => item.quantity > 0)
+    );
+  };
+
+  return (
+    <>
+      <h1> Proper Plants</h1>
+      <main>
+        <Plants plants={PLANTS} addToCart={addToCart} />
+        <Cart
+          cart={cart}
+          removeFromCart={removeFromCart}
+          addToCart={addToCart}
+        />
+      </main>
+    </>
+  );
 }
