e34cfb740457f522af07565196088fd92c4c30de
tornike thcalidze <ttchalidze@gmail.com>
2025-04-08 01:07:15 -0400

    first1

--- src/cart/cartItemQuantity.jsx
+++
@@ -1,9 +0,0 @@
-export default function CartItemQuantity({ item, removeFromCart, addToCart }) {
-  return (
-    <div className="cart-Quantity">
-      <button onClick={() => addToCart(item)}>+</button>
-      <span>{item.quantity}</span>
-      <button onClick={() => removeFromCart(item)}>-</button>
-    </div>
-  );
-}
